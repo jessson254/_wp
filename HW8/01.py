1b41e36cbfbcf570ab8300d34975ce7d900c1ae1
jessson254 <s111310537@student.nqu.edu.tw>
2025-06-08 23:16:39 +0800

    Create 01.py

---
+++ HW8/01.py
@@ -0,0 +1,10 @@
+def find_max(numbers):
+    if not numbers:
+        return None 
+    max_value = numbers[0]
+    for num in numbers:
+        if num > max_value:
+            max_value = num
+    return max_value
+
+print(find_max([1, 6, 3, 7, 2]))
