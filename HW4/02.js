4efd72437215c78af95f37d3aa9952d9684bb593
jessson254 <s111310537@student.nqu.edu.tw>
2025-06-08 22:38:35 +0800

    Update 02.js

--- HW4/02.js
+++ HW4/02.js
@@ -1,6 +1,8 @@
-function uniqueSorted(arr) {
+function uniqueSorted(arr) 
+{
   return [...new Set(arr)].sort((a, b) => a - b);
 }
 const arr = [5, 3, 8, 3, 1, 5, 2];
+
 console.log(uniqueSorted(arr)); 
-// [1, 2, 3, 5, 8]
+
